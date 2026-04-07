6df3a7cd5932075d2863b6f7cfb363f67c1e1208
Oudiematic3000 <oudmayera@gmail.com>
2026-04-07 11:51:45 +0200

    Fix HighlightType

--- IntoTheBeach/Assets/Scripts/Character/CharacterVisual.cs
+++ IntoTheBeach/Assets/Scripts/Character/CharacterVisual.cs
@@ -1,12 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 public class CharacterVisual : MonoBehaviour, Iinteractable
 {
     public static event Action<CharacterVisual> OnClick;
+    public int moveRange=1;
     public bool hasMoved, hasAttacked;
-   
+
+    private void OnEnable()
+    {
+        MovePlanTurnState.OnMovePlanStart += InitTile;
+    }
     public void OnHover(Vector2 mousePos)
     {
         print("My mom");
@@ -17,7 +23,15 @@ public class CharacterVisual : MonoBehaviour, Iinteractable
         OnClick?.Invoke(this);
     }
 
-   
+   public Vector3Int GetTilePos(Tilemap tilemap)
+    {
+        return tilemap.WorldToCell(transform.position);
+    }
 
+    public void InitTile()
+    {
+        Tilemap tilemap = FindFirstObjectByType<Tilemap>();
+        transform.position = tilemap.WorldToCell(transform.position);
+    }
 
 }
--- IntoTheBeach/Assets/Scripts/GridVisual.cs
+++ IntoTheBeach/Assets/Scripts/GridVisual.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -6,6 +7,11 @@ public class GridVisual : MonoBehaviour, Iinteractable
 {
     public Tilemap saloonTiles;
     public static event Action OnUnitMoved;
+
+    private void OnDisable()
+    {
+        UIActions.OnMovement -= HighlightMovableTiles;
+    }
     public void OnHover(Vector2 mousePos)
     {
         Vector3Int tilePos = saloonTiles.WorldToCell(mousePos);
@@ -35,6 +41,26 @@ public class GridVisual : MonoBehaviour, Iinteractable
         InputManager.Instance.SetState(InputManager.TurnState.None);
         OnUnitMoved?.Invoke();
     }
+    public List<Vector3Int> HighlightMovableTiles()
+    {
+        List<Vector3Int> highlightedTiles = new List<Vector3Int>();
+        var currentSelection = InputManager.Instance.GetCurrentSelection();
+        if (currentSelection == null) return highlightedTiles;
+        Vector3Int pos = currentSelection.GetTilePos(saloonTiles);
 
+        for(int x=pos.x-currentSelection.moveRange; x <= pos.x + currentSelection.moveRange; x++)
+        {
+
+            for (int y=pos.y-currentSelection.moveRange;y<=pos.y + currentSelection.moveRange; y++)
+            {
+                if(Math.Abs(x-pos.x)+Math.Abs(y-pos.y)>currentSelection.moveRange) continue;
+                if (Math.Abs(x - pos.x) + Math.Abs(y - pos.y) ==0) continue;
+                highlightedTiles.Add(pos);
+                saloonTiles.SetColor(new Vector3Int(x, y, 0), Color.rebeccaPurple);
+              
+            }
+        }
+        return highlightedTiles;
+    }
 
 }
