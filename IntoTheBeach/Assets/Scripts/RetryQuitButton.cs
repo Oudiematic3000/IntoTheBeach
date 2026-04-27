7540e97a9d622109504021ce38275b9155837d17
Oudiematic3000 <oudmayera@gmail.com>
2026-04-27 22:50:06 +0200

    Lobby fix

--- IntoTheBeach/Assets/Scripts/RetryQuitButton.cs
+++ IntoTheBeach/Assets/Scripts/RetryQuitButton.cs
@@ -1,16 +1,23 @@
+using System.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RetryQuitButton : NetworkBehaviour
 {
-    public void Retry() 
+    public async Task Retry() 
     {
-        if (NetworkManager.Singleton.IsServer)
+        //if (NetworkManager.Singleton.IsServer)
+        //{
+        //    NetworkManager.Singleton.SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+        //}
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening)
         {
-            NetworkManager.Singleton.SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+            NetworkManager.Singleton.Shutdown();
+            await Task.Yield();
         }
-       
+        SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+
     }
     public void Quit() 
     {
